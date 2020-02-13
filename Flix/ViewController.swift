eaf620d9fa3fbb996078f3974483171ee9e45a82
Rich Chau <richchau2022@gmail.com>
2020-02-13 08:23:18 -0800

    Initial Commit Pt2

---
+++ Flix/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  Flix
+//
+//  Created by Rich Chau on 2/4/20.
+//  Copyright © 2020 Rich Chau. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
