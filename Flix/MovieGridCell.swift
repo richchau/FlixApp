f69738c10608eeb10ec45a7a6b8e008604536a9e
Rich Chau <richchau2022@gmail.com>
2020-02-27 19:14:49 -0800

    Added extension to app to allow users to view details about movie. Implemented a  tab bar and grid view.

---
+++ Flix/MovieGridCell.swift
@@ -0,0 +1,14 @@
+//
+//  MovieGridCell.swift
+//  Flix
+//
+//  Created by Rich Chau on 2/27/20.
+//  Copyright © 2020 Rich Chau. All rights reserved.
+//
+
+import UIKit
+
+class MovieGridCell: UICollectionViewCell {
+    
+    @IBOutlet weak var posterView: UIImageView!
+}
